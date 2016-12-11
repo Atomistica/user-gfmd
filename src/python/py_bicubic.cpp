5233f6000b9340ad4d2383bd1424b1211ea95084
Lars Pastewka <lars.pastewka@kit.edu>
2016-12-11 13:55:09 +0100

    DOC: Minor cosmetics to docstring

--- src/python/py_bicubic.cpp
+++ src/python/py_bicubic.cpp
@@ -18,6 +18,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
    ====================================================================== */
+
 /*
  * A wrapper around Table2D to be used from within Python+Numpy
  */
