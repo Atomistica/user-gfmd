3d7d71bdbd1d35c319aaa56e7ff174fe6e80e766
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-04-01 16:45:35 +0200

    MAINT: Updated GFMD atom style

--- src/main/atom_vec_gfmd.h
+++ src/main/atom_vec_gfmd.h
@@ -47,39 +47,7 @@ namespace LAMMPS_NS {
 class AtomVecGFMD : public AtomVec {
  public:
   AtomVecGFMD(class LAMMPS *);
-  void grow(int);
-  void grow_reset();
-  void copy(int, int, int);
-  virtual int pack_comm(int, int *, double *, int, int *);
-  virtual int pack_comm_vel(int, int *, double *, int, int *);
-  virtual void unpack_comm(int, int, double *);
-  virtual void unpack_comm_vel(int, int, double *);
-  int pack_reverse(int, int, double *);
-  void unpack_reverse(int, int *, double *);
-  virtual int pack_border(int, int *, double *, int, int *);
-  virtual int pack_border_vel(int, int *, double *, int, int *);
-  virtual void unpack_border(int, int, double *);
-  virtual void unpack_border_vel(int, int, double *);
-  virtual int pack_exchange(int, double *);
-  virtual int unpack_exchange(double *);
-  int size_restart();
-  int pack_restart(int, double *);
-  int unpack_restart(double *);
-  void create_atom(int, double *);
-  void data_atom(double *, int, char **);
-  void pack_data(double **);
-  void write_data(FILE *, int, double **);
-  int data_atom_hybrid(int, char **);
-  bigint memory_usage();
-
- protected:
-  int *tag,*type,*mask,*image;
-  bigint *gid;
-  double **x,**v,**f;
-  double **xeq;
 };
-
-}
 
 #endif
 #endif
