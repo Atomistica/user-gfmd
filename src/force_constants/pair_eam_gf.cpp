406bdda133b8894064fa2f7b8b9b5fff02268f0e
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-10-14 12:33:57 +0200

    MAINT: Removed  suffix to comm method

--- src/force_constants/pair_eam_gf.cpp
+++ src/force_constants/pair_eam_gf.cpp
@@ -134,7 +134,7 @@ void PairEAMGF::compute(int eflag, int vflag)
 
   // communicate and sum densities
 
-  if (newton_pair) comm->reverse_comm_pair(this);
+  if (newton_pair) comm->reverse_comm(this);
 
   // fp = derivative of embedding energy at each atom
   // phi = embedding energy at each atom
@@ -163,7 +163,7 @@ void PairEAMGF::compute(int eflag, int vflag)
 
   // communicate derivative of embedding function
 
-  comm->forward_comm_pair(this);
+  comm->forward_comm(this);
   embedstep = update->ntimestep;
 
   // compute forces on each atom
